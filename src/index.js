2ea0b9e452f489df4fcea068ce35ee03478452d3
yama-choki <82372881+yama-choki@users.noreply.github.com>
2022-07-11 23:40:43 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const App = () => {
+  return (
+    <>
+      <h1>こんにちは</h1>
+    </>
+  );
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
